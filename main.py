0841259b4179617e90ecb151a5a5d06b3329324a
kaza1609 <azabelina633@gmail.com>
2024-10-24 18:28:30 +0700

    Начинаем заново

---
+++ main.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+
+
+def calculate_balance(income, expenses):
+    return income + expenses
+
+
+def on_calculate():
+    income = float(entry_income.get())
+    expenses = float(entry_expenses.get())
+    balance = calculate_balance(income, expenses)
+    label_result.config(text=f"бюджddеdddтffdddfffddff: {balance}")
+
+
+if __name__ == "__main__":
+    app = tk.Tk()
+    app.title("Калькулятор финансов")
+
+    tk.Label(app, text="Доходы:").pack()
+    entry_income = tk.Entry(app)
+    entry_income.pack()
+
+    tk.Label(app, text="Расходы:").pack()
+    entry_expenses = tk.Entry(app)
+    entry_expenses.pack()
+
+    tk.Button(app, text="Подсчитать", command=on_calculate).pack()
+    label_result = tk.Label(app, text="")
+    label_result.pack()
+
+    app.mainloop()
---
+++ test_main.py
@@ -0,0 +1,7 @@
+import pytest
+from main import calculate_balance
+
+def test_calculate_balance():
+    assert calculate_balance(1000, 500) == 1500
+    assert calculate_balance(500, 1000) == 1500
+

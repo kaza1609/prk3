828c587e77eecff192c78a2e60c72884b0a16c17
kaza1609 <azabelina633@gmail.com>
2024-10-31 11:49:29 +0700

    fdf

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def on_calculate():
     income = float(entry_income.get())
     expenses = float(entry_expenses.get())
     balance = calculate_balance(income, expenses)
-    label_result.config(text=f"ввfdвававмаваfdввdт: {balance}")
+    label_result.config(text=f"ввfdвававмаваcdcdfdввdт: {balance}")
 
 
 if __name__ == "__main__":

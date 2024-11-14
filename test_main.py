dd01149740937cbb543e4e3bb2f40e4ba83c035a
kaza1609 <azabelina633@gmail.com>
2024-11-14 14:06:01 +0700

    df

--- test_main.py
+++ test_main.py
@@ -3,5 +3,5 @@ from main import calculate_balance
 
 def test_calculate_balance():
     assert calculate_balance(1000, 500) == 1500
-    assert calculate_balance(500, 500) == 1500
+    assert calculate_balance(500, 1500) == 2000
 

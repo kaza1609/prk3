1eb478150289df6857efab652d4b3f3b08f309cc
kaza1609 <azabelina633@gmail.com>
2024-11-14 17:43:29 +0700

    broke

--- test_main.py
+++ test_main.py
@@ -2,6 +2,6 @@ import pytest
 from main import calculate_balance
 
 def test_calculate_balance():
-    assert calculate_balance(1000, 500) == 1500
+    assert calculate_balance(100000, 500) == 1500
     assert calculate_balance(500, 1500) == 2000
 
